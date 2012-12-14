08c761937ae59cf934cf41ac275ee6a45de0e2f3
wim033 <william_mckinney@harvard.edu>
2012-12-14 10:28:08 -0500

    another attempt at phonegap build + childbrowser

--- www/childbrowser.js
+++ www/childbrowser.js
@@ -1,87 +1,115 @@
-/* MIT licensed */
-// (c) 2010 Jesse MacFadyen, Nitobi
+/*
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ *
+ * Copyright 2012, Andrew Lunny, Adobe Systems
+ * Copyright (c) 2005-2010, Nitobi Software Inc.
+ * Copyright (c) 2011, IBM Corporation
+ * (c) 2010 Jesse MacFadyen, Nitobi
+ */
 
-
-(function() {
-
-    var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
-
-    function ChildBrowser() {
-        // Does nothing
+var ChildBrowser = (function (gap) {
+    function isFunction(f) {
+        return typeof f === "function";
     }
 
-// Callback when the location of the page changes
-// called from native
-    ChildBrowser._onLocationChange = function(newLoc)
-    {
-        window.plugins.childBrowser.onLocationChange(newLoc);
-    };
+    // placeholder and constants
+    function ChildBrowser() {}
 
-// Callback when the user chooses the 'Done' button
-// called from native
-    ChildBrowser._onClose = function()
-    {
-        window.plugins.childBrowser.onClose();
-    };
+    var CLOSE_EVENT = 0,
+        LOCATION_CHANGED_EVENT = 1,
+        OPEN_EXTERNAL_EVENT = 2;
 
-// Callback when the user chooses the 'open in Safari' button
-// called from native
-    ChildBrowser._onOpenExternal = function()
-    {
-        window.plugins.childBrowser.onOpenExternal();
-    };
-
-// Pages loaded into the ChildBrowser can execute callback scripts, so be careful to
-// check location, and make sure it is a location you trust.
-// Warning ... don't exec arbitrary code, it's risky and could fuck up your app.
-// called from native
-    ChildBrowser._onJSCallback = function(js,loc)
-    {
-        // Not Implemented
-        //window.plugins.childBrowser.onJSCallback(js,loc);
-    };
-
-    /* The interface that you will use to access functionality */
-
-// Show a webpage, will result in a callback to onLocationChange
-    ChildBrowser.prototype.showWebPage = function(loc)
-    {
-        cordovaRef.exec("ChildBrowserCommand.showWebPage", loc);
-    };
-
-// close the browser, will NOT result in close callback
-    ChildBrowser.prototype.close = function()
-    {
-        cordovaRef.exec("ChildBrowserCommand.close");
-    };
-
-// Not Implemented
-    ChildBrowser.prototype.jsExec = function(jsString)
-    {
-        // Not Implemented!!
-        //PhoneGap.exec("ChildBrowserCommand.jsExec",jsString);
-    };
-
-// Note: this plugin does NOT install itself, call this method some time after deviceready to install it
-// it will be returned, and also available globally from window.plugins.childBrowser
-    ChildBrowser.install = function()
-    {
-        if(!window.plugins) {
-            window.plugins = {};
+    /**
+     * Function called when the child browser has an event.
+     */
+    function onEvent(data) {
+        switch (data.type) {
+            case CLOSE_EVENT:
+                if (isFunction(ChildBrowser.onClose)) {
+                    ChildBrowser.onClose();
+                }
+                break;
+            case LOCATION_CHANGED_EVENT:
+                if (isFunction(ChildBrowser.onLocationChange)) {
+                    ChildBrowser.onLocationChange(data.location);
+                }
+                break;
+            case OPEN_EXTERNAL_EVENT:
+                if (isFunction(ChildBrowser.onOpenExternal)) {
+                    ChildBrowser.onOpenExternal();
+                }
+                break;
         }
-        if ( ! window.plugins.childBrowser ) {
-            window.plugins.childBrowser = new ChildBrowser();
-        }
-
-    };
-
-
-    if (cordovaRef && cordovaRef.addConstructor) {
-        cordovaRef.addConstructor(ChildBrowser.install);
-    } else {
-        console.log("ChildBrowser Cordova Plugin could not be installed.");
-        return null;
     }
 
+    /**
+     * Function called when the child browser has an error.
+     */
+    function onError(data) {
+        if (isFunction(ChildBrowser.onError)) {
+            ChildBrowser.onError(data);
+        }
+    }
 
-})();
+    /**
+     * Maintain API consistency with iOS
+     */
+    ChildBrowser.install = function () {
+        console.log('ChildBrowser.install is deprecated');
+    }
+
+    /**
+     * Display a new browser with the specified URL.
+     * This method loads up a new web view in a dialog.
+     *
+     * @param url           The url to load
+     * @param options       An object that specifies additional options
+     */
+    ChildBrowser.showWebPage = function (url, options) {
+        if (!options) {
+            options = { showLocationBar: true };
+        }
+
+        gap.exec(onEvent, onError, "ChildBrowser", "showWebPage", [url, options]);
+    };
+
+    /**
+     * Close the browser opened by showWebPage.
+     */
+    ChildBrowser.close = function () {
+        gap.exec(null, null, "ChildBrowser", "close", []);
+    };
+
+    /**
+     * Display a new browser with the specified URL.
+     * This method starts a new web browser activity.
+     *
+     * @param url           The url to load
+     * @param usePhoneGap   Load url in PhoneGap webview [optional]
+     */
+    ChildBrowser.openExternal = function(url, usePhoneGap) {
+        if (usePhoneGap) {
+            navigator.app.loadUrl(url);
+        } else {
+            gap.exec(null, null, "ChildBrowser", "openExternal", [url, usePhoneGap]);
+        }
+    };
+
+    /**
+     * Load ChildBrowser
+     */
+    gap.addConstructor(function () {
+        if (gap.addPlugin) {
+            gap.addPlugin("childBrowser", ChildBrowser);
+        } else {
+            if (!window.plugins) {
+                window.plugins = {};
+            }
+
+            window.plugins.childBrowser = ChildBrowser;
+        }
+    });
+
+    return ChildBrowser;
+})(window.Cordova || window.PhoneGap);
